779c5557af0ba933ceb54d1d9f830b2aa5ffdc7d
Imam26 <i.m.a.m.sabitov@mail.ru>
2019-02-06 17:53:44 +0600

    second commit

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,6 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using SharpVectors.Converters;
+using SharpVectors.Renderers.Wpf;
 
 namespace WpfApp2
 {
@@ -22,7 +28,107 @@ namespace WpfApp2
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int days = 4;
+
+            List<PlaceCoord> placeCoords = new List<PlaceCoord>();
+            placeCoords.Add(new PlaceCoord { Name="Астана", Lat = "51.1801000", Lon= "71.4459800" });
+            placeCoords.Add(new PlaceCoord { Name = "Алматы", Lat = "43.2566700", Lon = "76.9286100" });
+            placeCoords.Add(new PlaceCoord { Name = "Павлодар", Lat = "52.2833300", Lon = "76.9666700" });
+            placeCoords.Add(new PlaceCoord { Name = "Экибастуз", Lat = "51.7237100", Lon = "75.3228700" });
+            placeCoords.Add(new PlaceCoord { Name = "Кокшетау", Lat = "53.2833300", Lon = "69.4000000" });
+
+            string selectedItem = ((ComboBoxItem)cityList.SelectedItem).Content.ToString();
+            PlaceCoord findPlaceCoord = placeCoords.Find(item => item.Name == selectedItem);
+
+            if (findPlaceCoord == null)
+            {
+                MessageBox.Show("В БД отсутсвует информация по данной местности");
+                return;
+            }
+
+            string allDataRequestUriStr = "https://api.weather.yandex.ru/v1/forecast?lat=" + findPlaceCoord.Lat + "&lon=" + findPlaceCoord.Lon
+                + "&lang=ru_RU&limit=7&hours=false&extra=false";
+
+                  
+            WebRequest request = WebRequest.Create(allDataRequestUriStr);
+            request.Headers.Add("X-Yandex-API-Key: 99e485d2-7855-440e-a07c-f7ca00df1faa");
+            WebResponse response = request.GetResponse();
+
+            dynamic allData;
+            List<string> iconsXML = new List<string>();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    allData = JsonConvert.DeserializeObject(reader.ReadToEnd());                            
+                }
+            }
+
+            SetImageElement(allData);
+            SetWeatherInfoBlock(allData.forecasts);
+        }
+
+        private void CityList_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SetWeatherInfoBlock(dynamic data)
+        {
+            var textBlocks = weatherInfoGrid.Children.OfType<TextBlock>();
+
+            for(int i = 0; i<textBlocks.Count(); i++)
+            {
+                textBlocks.ElementAt(i).Text = "Дата: " + data[i].date + "\n"
+                        + "Температура воздуха: " + data[i].parts.day_short.temp + "\n"
+                        + "Ощущается: " + data[i].parts.day_short.feels_like + "\n"
+                        + "Скорость ветра: " + data[i].parts.day_short.wind_speed + " м/с\n"
+                        + "Влажность воздуха: " + data[i].parts.day_short.humidity + " %\n";
+            }
+        }
+
+        private void SetImageElement(dynamic allData)
+        {
+            WpfDrawingSettings settings = new WpfDrawingSettings();
+            settings.IncludeRuntime = true;
+            settings.TextAsGeometry = false;
+
+            StreamSvgConverter converter = new StreamSvgConverter(settings);                        
+
+            WebRequest request;
+
+            var imagesElement = weatherInfoGrid.Children.OfType<Image>();
+
+            for (int i = 0; i < imagesElement.Count(); i++)
+            {
+                string iconDataRequestUriStr = "https://yastatic.net/weather/i/icons/blueye/color/svg/"
+                    + allData.forecasts[i].parts.day_short.icon + ".svg";
+
+                request = WebRequest.Create(iconDataRequestUriStr);
+
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (Stream stream = request.GetResponse().GetResponseStream())
+                    {
+                        if (converter.Convert(stream, memStream))
+                        {
+                            BitmapImage bitmap = new BitmapImage();
+                            bitmap.BeginInit();
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmap.StreamSource = memStream;
+                            bitmap.EndInit();
+
+                            imagesElement.ElementAt(i).Source = bitmap;
+                        }
+                    }
+                }                                       
+            }            
         }
     }
 }
---
+++ WpfApp2/PlaceCoord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class PlaceCoord
+    {
+        public string Name { get; set; }
+        //Широта
+        public string Lat { get; set; }
+        //Долгота
+        public string Lon { get; set; }
+    }
+}
